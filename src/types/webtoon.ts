65843c6d96edbd5873d33c289d35bd48a3bd3bf5
Hyeokjae Lee (Nagle) <leehyeokjae97@gmail.com>
2021-10-22 10:40:53 +0900

    🌱 feat: kakao webtoon cralwer 추가
    * 프로젝트 용도 변경
    * 기반 코드 추가

---
+++ src/types/webtoon.ts
@@ -0,0 +1,9 @@
+export interface Webtoon {
+  title: string | undefined;
+  artist: string | undefined;
+  url: string | undefined;
+  img: string | undefined;
+  service: string | undefined;
+  weekday: number | undefined;
+  adult: boolean | undefined;
+}
